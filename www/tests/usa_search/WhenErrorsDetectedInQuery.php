2369d48a6563ee549793117342c80abdd4ffde88
Brian Chiasson <bchiasson@ubuntu.(none)>
2011-07-14 18:29:22 -0700

    BC - Added test case for when no searchTerm or autoDetails provided.

--- www/tests/usa_search/WhenErrorsDetectedInQuery.php
+++ www/tests/usa_search/WhenErrorsDetectedInQuery.php
@@ -1,15 +1,21 @@
 <?php
 require_once '../simpletest/autorun.php';
 require_once '../../usa_search/UsaSearch.php';
+require_once '../../usa_search/Query.php';
 
 class WhenErrorsDetectedInQuery extends UnitTestCase {
 
 	function testExceptionThrownForNoQueryEntity() {
 		$api = new UsaSearch();
 		$this->expectException(new InvalidArgumentException('A Query entity must be provided to perform the search operation.'));
-		$api->search(NULL);	
+		$api->search(NULL);
 	}
 	
+	function testExceptionThrownForQueryEntityWithoutSearchTermOrAutoDetails() {
+		$api = new UsaSearch();
+		$this->expectException(new InvalidArgumentException('The query must have a searchTerm or autoDetails provided.'));
+		$api->search(new Query());
+	}
 }
 
 ?>
--- www/usa_search/UsaSearch.php
+++ www/usa_search/UsaSearch.php
@@ -1,7 +1,13 @@
 <?php
+
 class UsaSearch {
 	function search($query) {
+		$this->validate($query);
+	}
+	
+	private function validate($query) {
 		if (is_null($query)) throw new InvalidArgumentException('A Query entity must be provided to perform the search operation.');
+		if (empty($query->searchTerm)) throw new InvalidArgumentException('The query must have a searchTerm or autoDetails provided.');
 	}
 }
 ?>
