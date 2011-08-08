a991676612a70c8b5ae365fcf257c1d15cf62ed8
Brian Chiasson <bchiasson@ubuntu.(none)>
2011-08-08 00:35:15 -0400

    BC - Implemented the activate member.

--- www/activate.php
+++ www/activate.php
@@ -1,5 +1,7 @@
 <?php
 
+$pageName = 'activate';
+require_once('classes/FlashMessenger.php');
 require_once('utils/forms.php');
 include('templates/header.php'); 
 
@@ -8,24 +10,29 @@ include('templates/header.php');
 <h3>Membership Activation</h3>
 <p>Please enter the account activation information below.</p>
 
+
+
+<?php rasFlash(); ?>
+
 <form id="memberActivationForm" action="activateMember.php" method="post">
+	<p>
+		<?php echo rasRequiredLabel("Email", "memberEmail"); ?><br />
+		<?php echo rasRequiredTextField(array(
+			 'name' => 'memberEmail'
+			,'class' => 'email'
+			,'id' => 'memberEmail'
+			,'maxlength' => 256
+			,'value' => $_GET['e']
+		)); ?>
+	</p>
 	<p>
 		<?php echo rasRequiredLabel("Token", "activationToken"); ?><br />
 		<?php echo rasRequiredTextField(array(
 			 'name' => 'activationToken'
 			,'id' => 'activationToken'
 			,'maxlength' => 256
+			,'value' => $_GET['t']
 		)); ?>
-	</p>
-	<p>
-		<?php echo rasRequiredLabel("Password", "memberPassword"); ?><br />
-		<?php echo rasRequiredPassword(array(
-			 'name' => 'memberPassword'
-			,'id' => 'memberPassword'
-			,'maxlength' => 256
-		)); ?><br />
-		<?php echo rasRequiredLabel("Confirm Password", "memberPasswordConfirm"); ?><br />
-		<?php echo rasPasswordConfirmation('memberPassword'); ?>
 	</p>
 	<input type="submit" value="Activate" />
 </form>
